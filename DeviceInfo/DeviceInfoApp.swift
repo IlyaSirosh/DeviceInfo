e3fbaeca8eb96dcc2fab82f2bfdf1b34b695372e
Ilya Sirosh <sirosh.ilya@gmail.com>
2021-02-03 06:55:06 +0200

    Initial Commit

---
+++ DeviceInfo/DeviceInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoApp.swift
+//  DeviceInfo
+//
+//  Created by Illya Sirosh on 03.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
